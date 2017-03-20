d1d095f89b1a68119db1ee0fd5d7763fd72ae328
Matthew Rossman <rossman.m@husky.neu.edu>
2017-03-19 21:42:15 -0400

    Expanded complex class, added Mandlebrot calculation and framework for fractal painter

--- Complex.java
+++ Complex.java
@@ -3,16 +3,54 @@
 
 public class Complex {
 
-   private double r;
-   private double i;
+   private double x,y;
    
-   public Complex(double real, double imaginary){
-      r = real;
-      i = imaginary;
+   public Complex(){}
+   
+   public Complex(double x, double y){
+      this.x = x;
+      this.y = y;
    }
    
    public double abs(){
-       return Math.sqrt(r*r + i*i);
+       return Math.sqrt(x*x+y*y);
+   }
+   
+   public double getX(){ return x; }
+   public double getY(){ return y; }
+   
+   public void setX(double x){ this.x=x; }
+   public void setY(double y){ this.y=y; }
+   
+   public int iterateMandlebrot(int totalIterations){
+      Complex z = new Complex(0,0);
+      int iterationCount = 0;
+      do {
+         z = multiply(z,z);
+         z = add(z,this);
+         iterationCount++;
+      } while (z.abs()<2 && iterationCount<=totalIterations);
+      
+      return iterationCount;
+   
+   }
+   
+   public static Complex multiply(Complex a, Complex b){
+      Complex out = new Complex();
+      
+      out.setX(a.getX()*b.getX()-a.getY()*b.getY());
+      out.setY(a.getX()*b.getY()+a.getY()*b.getX());
+      
+      return out;
+   }
+   
+   public static Complex add(Complex a, Complex b) {
+      Complex out = new Complex();
+      
+      out.setX(a.getX()+b.getX());
+      out.setY(a.getY()+b.getY());
+      
+      return out;
    }
 
 }
--- FractalPanel.java
+++ FractalPanel.java
@@ -1,7 +1,50 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class FractalPanel extends JPanel { 
 
-   public FractalPanel() { }
+   private double graphWidth = 3;
+   private double graphHeight = graphWidth;
+   private Complex origin = new Complex(-2,-1.5);
+   
+   int pixSize = 1;
+   int maxIterations = 15;
+
+   public FractalPanel() {
+   
+   
+   }
+   
+   public void paintComponent(Graphics g){
+      super.paintComponent(g);
+      
+      drawFractal(g);
+   }
+   
+   private void drawFractal(Graphics g){
+      Complex thisPoint;
+      for (int x=pixSize/2; x<getWidth(); x+=pixSize){
+         for (int y=pixSize/2; y<getHeight(); y+=pixSize){
+            thisPoint = new Complex(origin.getX()+(double)x/getWidth()*graphWidth,
+                                    origin.getY()+(double)y/getHeight()*graphHeight);
+            
+            setFractalColor(g,thisPoint);
+            
+            g.fillRect(x-pixSize/2,y-pixSize/2,pixSize,pixSize);      
+         
+         }
+      
+      }
+   }
+   
+   private void setFractalColor(Graphics g, Complex c) {
+      if (c.iterateMandlebrot(maxIterations)<=maxIterations)
+         g.setColor(Color.RED);
+      else
+         g.setColor(Color.BLACK);
+   }
 
 }
